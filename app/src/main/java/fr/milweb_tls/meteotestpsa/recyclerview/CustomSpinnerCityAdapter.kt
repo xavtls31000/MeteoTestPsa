ab3b800922f8d2f28f7a7cb2f9498b014a5c94d1
xavtl <xavtls31000@gmail.com>
2022-09-18 19:59:25 +0200

    - add img weather in spinner
    working finished

--- app/src/main/java/fr/milweb_tls/meteotestpsa/recyclerview/CustomSpinnerCityAdapter.kt
+++ app/src/main/java/fr/milweb_tls/meteotestpsa/recyclerview/CustomSpinnerCityAdapter.kt
@@ -1,5 +1,6 @@
 package fr.milweb_tls.meteotestpsa.recyclerview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,12 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import fr.milweb_tls.meteotestpsa.R
+import fr.milweb_tls.meteotestpsa.base.BaseActivity
 import fr.milweb_tls.meteotestpsa.entities.City
+import fr.milweb_tls.meteotestpsa.reposytory.WeatherRepository
+import fr.milweb_tls.meteotestpsa.util.ImgageMeteo
 
 class CustomSpinnerCityAdapter(context: Context, listCity: List<City>):
       ArrayAdapter<City>(context,0,listCity){
@@ -24,6 +29,7 @@ class CustomSpinnerCityAdapter(context: Context, listCity: List<City>):
 
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun listView(position: Int, convertView: View?, parent: ViewGroup): View {
 
         val list = getItem(position)
@@ -33,6 +39,11 @@ class CustomSpinnerCityAdapter(context: Context, listCity: List<City>):
             val txt = view.findViewById<TextView>(R.id.txt_spinner)
             val weatherSpinner = view.findViewById<ImageView>(R.id.weather_spinner)
             txt.text=list!!.name
+            val cityWeather = WeatherRepository(BaseActivity.databaseRoom.weatherDao()).getWeatherForCity(it!!.name)
+            if (cityWeather!=null){
+                val image = ImgageMeteo().getDataMeteo(cityWeather.icon)
+                weatherSpinner.setImageDrawable(context.getDrawable(image.main_image))
+            } else weatherSpinner.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_meteo))
 
         }
         return view
